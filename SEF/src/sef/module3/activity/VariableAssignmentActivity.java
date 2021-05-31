a021cd6d13a8be1f42b64d5b537fbb8a5cceb012
eldarveromej <80579777+eldarveromej@users.noreply.github.com>
2021-06-01 00:33:31 +0300

    Update VariableAssignmentActivity.java

--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -17,6 +17,16 @@ public class VariableAssignmentActivity {
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
 		// 3- Print updated value to the console
+		int a = 1;
+		double b = a;
+		double c = 1.5;
+		int d;
+		d=(int)c;
+		do {
+			++d;
+			System.out.println(d);
+		} while (d<10 || d>0);
+		System.out.println(d);
 	}
 
 }
