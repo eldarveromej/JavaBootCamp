ee837cbb23768031b90ce31c9069e6c5f7dd39af
eldarveromej <80579777+eldarveromej@users.noreply.github.com>
2021-06-01 00:28:54 +0300

    Update PrintNumWithWhile.java

--- SEF/src/sef/module3/activity/PrintNumWithWhile.java
+++ SEF/src/sef/module3/activity/PrintNumWithWhile.java
@@ -1,36 +1,41 @@
 /**
- * 
+ *
  */
 package sef.module3.activity;
 
 /**
- * @author 
+ * @author
  *
  */
 public class PrintNumWithWhile {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		
-		// Print all even numbers less than 100
-//		int i = 0;
-//		while(i<100){
-//			System.out.println(i);
-//			i=i+2;			
-//		}
-//		
-		 
-		// write code to Print all odd numbers less than 100
-		int j = 100;
-		while(j < 100) {
-			System.out.println(j);
-			j = j +2;
-			
-		}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-	}
 
+        // Print all even numbers less than 100
+        int i = 0;
+        while (i < 100) {
+            System.out.println(i);
+            i = i + 2;
+        }
+
+
+        // write code to Print all odd numbers less than 100
+        int j = 100;
+        while (j < 100) {
+            System.out.println(j);
+            j = j + 2;
+
+        }
+        int x = 0;
+        while (x < 100) {
+            System.out.println(x);
+            x += 2;
+        }
+
+    }
 }
+
