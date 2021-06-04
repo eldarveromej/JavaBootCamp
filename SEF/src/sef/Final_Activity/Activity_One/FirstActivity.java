73b86600de5b2bf261893847109e202f881b244e
eldarveromej <80579777+eldarveromej@users.noreply.github.com>
2021-06-04 18:59:39 +0300

    Update FirstActivity.java

--- SEF/src/sef/Final_Activity/Activity_One/FirstActivity.java
+++ SEF/src/sef/Final_Activity/Activity_One/FirstActivity.java
@@ -9,10 +9,10 @@ public class FirstActivity {
 
         Employee dobro = new Employee("Eldar", 24, "QA", "Accenture", 7000);
         Employee belka = new Employee("Elizabeth", 18, "Face", "Tiktok", 4000);
-        Employee kakai = new Employee("Akaki", 44, "QA", "Rosgosstrakh", 5000);
+        Employee kakai = new Employee("Akaki", 44, "USSR", "Rosgosstrakh", 5000);
         Employee tomsk = new Employee("Dobrynya", 21, "QA", "Scandiweb", 5000);
         Employee april = new Employee("Mirella", 23, "QA", "Accenture", 3500);
-        Employee melon = new Employee("Timur", 36, "QA", "Decta", 6000);
+        Employee melon = new Employee("Timur", 36, "HR Manager", "Decta", 6000);
 
         List<Employee> employeeList = new ArrayList<>();
         employeeList.add(dobro);
