a55857d3b08baca1f716d7422c91d0eb495f3f41
eldarveromej <80579777+eldarveromej@users.noreply.github.com>
2021-06-06 13:05:41 +0300

    Update FirstActivity.java

--- SEF/src/sef/Final_Activity/Activity_One/FirstActivity.java
+++ SEF/src/sef/Final_Activity/Activity_One/FirstActivity.java
@@ -1,18 +1,17 @@
 package sef.Final_Activity.Activity_One;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class FirstActivity {
     public static void main(String[] args) {
 
 
-        Employee dobro = new Employee("Eldar", 24, "QA", "Accenture", 7000);
-        Employee belka = new Employee("Elizabeth", 18, "Face", "Tiktok", 4000);
-        Employee kakai = new Employee("Akaki", 44, "USSR", "Rosgosstrakh", 5000);
-        Employee tomsk = new Employee("Dobrynya", 21, "QA", "Scandiweb", 5000);
-        Employee april = new Employee("Mirella", 23, "QA", "Accenture", 3500);
-        Employee melon = new Employee("Timur", 36, "HR Manager", "Decta", 6000);
+        Employee dobro = new Employee("Eldar", "Veromej", 24, "QA", "Accenture", 7000);
+        Employee belka = new Employee("Elizabeth", "Windsor", 18, "Face", "Tiktok", 4000);
+        Employee kakai = new Employee("Akaki", "Paki", 44, "Mayor", "Rosgosstrakh", 5000);
+        Employee tomsk = new Employee("Dobrynya", "Nikitich",21, "QA", "Scandiweb", 5000);
+        Employee april = new Employee("Mirella","Pampella", 23, "QA", "Accenture", 3500);
+        Employee melon = new Employee("Timur","Parkur", 36, "QA", "Decta", 6000);
 
         List<Employee> employeeList = new ArrayList<>();
         employeeList.add(dobro);
@@ -22,13 +21,13 @@ public class FirstActivity {
         employeeList.add(april);
         employeeList.add(melon);
 
+        employeeList.forEach(Employee::setintroducePerson);
+
         employeeList.sort(Comparator.comparingInt(Employee::getSalary).reversed());
 
         for(Employee employee : employeeList) {
             System.out.println(employee);
         }
-
-        employeeList.forEach(Employee::introducePerson);
     }
 }
 
