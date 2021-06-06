530829692e9b72b032824046acbf0a7ec9b1d5b5
Eldars Veromejs <eldarveromej@gmail.com>
2021-06-06 12:55:43 +0300

    Activity 1, ver.: 2.0.

--- SEF/src/sef/Final_Activity/Activity_One/Student.java
+++ SEF/src/sef/Final_Activity/Activity_One/Student.java
@@ -1,24 +1,23 @@
 package sef.Final_Activity.Activity_One;
 
-    public class Student extends Person {
+public class Student extends Person {
 
-        private String schoolName;
+    private String schoolName;
 
-        public Student(String name, int age) {
-            super(name, age);
-        }
-
-        public String getSchoolName() {
-            return schoolName;
-        }
-
-        public void setSchoolName(String schoolName) {
-            this.schoolName = schoolName;
-        }
-
-        @Override
-        public void introducePerson() {
-            System.out.printf("I study in university %d", schoolName);
-        }
+    public Student(String name, String surname, int age) {
+        super(name, surname,  age);
     }
 
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    @Override
+    public String introducePerson() {
+        return String.format("I study in university %d", schoolName);
+    }
+}
