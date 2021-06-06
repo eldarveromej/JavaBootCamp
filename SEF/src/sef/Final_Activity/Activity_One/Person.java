a8c9d143da6ac39803ad3469b9eb1a8cd5ac4454
eldarveromej <80579777+eldarveromej@users.noreply.github.com>
2021-06-06 13:06:08 +0300

    Update Person.java

--- SEF/src/sef/Final_Activity/Activity_One/Person.java
+++ SEF/src/sef/Final_Activity/Activity_One/Person.java
@@ -1,12 +1,16 @@
 package sef.Final_Activity.Activity_One;
 
+import sef.Final_Activity.Activity_Two.PersonException;
+
 public class Person {
-    public Person(String name, int age) {
+    public Person(String name, String surname, int age) {
         this.name = name;
+        this.surname = surname;
         this.age = age;
     }
 
     private String name;
+    private String surname;
     private int age;
 
     public String getName() {
@@ -25,9 +29,26 @@ public class Person {
         this.age = age;
     }
 
-    public void introducePerson() {
-        System.out.printf("My name is %s and I am %d years old", getName(), getAge());
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String introducePerson() {
+        return String.format("My name is %s and I am %d years old", getName(), getAge());
+    }
+    public void setintroducePerson() {
+        System.out.printf("My name is %s and I am %d years old " + "\n", getName(), getAge());
+    }
+
+    public String checkIfContainsNumbers(String str) {
+        if (str.matches("[a-zA-Z]+")) {
+            return str;
+        } else {
+            throw new PersonException(String.format("Provided string: %s contains number", str));
+        }
     }
 }
-
-
